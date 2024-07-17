76d6e836040332cdd3756613213072f3ad6e57be
Jia_RG <jiasust@foxmail.com>
2024-07-18 03:14:12 +0800

    Fixed Void Monolith view jump to Anomaly research window wrong(jumped to research tree view)

---
+++ Source/ResearchTree/Patches/DiaOption_Activate.cs
@@ -0,0 +1,24 @@
+using System;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace FluffyResearchTree;
+
+[HarmonyPatch(typeof(DiaOption), nameof(DiaOption.Activate))]
+public class DiaOption_Activate
+{
+    private static void Prefix(string ___text)
+    {
+        if (!"VoidMonolithViewResearch".Translate().ToString().Equals(___text))
+        {
+            return;
+        }
+        // fixed Void Monolith view jump to Anomaly research window wrong(jumped to research tree view)
+        // Because in the original code, the research window is jumped first and then the tab of the window is changed.
+        // This patch just changes the tab in advance, and subsequent patch files will be able to correctly judge
+        // see: MainTabsRoot_Patches
+        ((MainTabWindow_Research) MainButtonDefOf.Research.TabWindow).CurTab = ResearchTabDefOf.Anomaly;
+    }
+}
